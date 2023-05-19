67a083126f1a538b34fa2d346abff3a1febc79f8
jhlee <wisdom_13_@naver.com>
2023-05-19 15:28:22 +0900

    이전, 다음 포스트 링크

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { getPostData } from '@/service/posts';
 import { redirect } from 'next/navigation';
 import PostContent from '@/components/PostContent';
+import AdjacentPostCard from '@/components/AdjacentPostCard';
 
 type Props = {
   params: {
@@ -18,11 +19,15 @@ export default async function page({ params: { slug } }: Props) {
   }
 
   return (
-    <article className='w-full shadow-xl rounded-md mt-6'>
+    <article className='w-full shadow-xl rounded-md mt-6 overflow-hidden'>
       <div className='overflow-hidden h-1/5 max-h-[300px]'>
         <Image className='rounded-md w-full' src={`/images/posts/${post.path}.png`} alt={post.path} width={760} height={420} />
       </div>
       <PostContent post={post} />
+      <section className='flex'>
+        {post.prev && <AdjacentPostCard post={post.prev} type='prev' />}
+        {post.next && <AdjacentPostCard post={post.next} type='next' />}
+      </section>
     </article>
   );
 }
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,7 +10,11 @@ export type Post = {
   featured: string;
 }
 
-export type PostData = Post & { content: string };
+export type PostData = Post & {
+  content: string;
+  next: Post | null;
+  prev: Post | null
+};
 
 export async function getFeaturedPosts(): Promise<Post[]> {
   return getAllPosts()
@@ -26,11 +30,17 @@ export async function getAllPosts(): Promise<Post[]> {
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), 'data/posts', `${fileName}.md`);
-  const metadata = await getAllPosts()
-    .then((post) => post.find((post) => post.path === fileName));
-  if (!metadata)
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
+
+  if (!post)
     throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
 
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length - 1 ? posts[index + 1] : null;
+
+
   const content = await readFile(filePath, 'utf-8');
-  return { ...metadata, content };
+  return { ...post, content, next, prev };
 } 

6f1ee9b603ccf81b671626288bd7c077531fefb8
jhlee <wisdom_13_@naver.com>
2023-05-20 14:48:02 +0900

    Contact 입력폼

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,21 @@
 import Contact from "@/components/Contact";
+import { AiFillGithub, AiOutlineTwitter, AiFillMail } from 'react-icons/ai';
+
 
 export default function ContactPage() {
   return (
-    <div>
+    <div className='flex flex-row shadow-xl rounded-md mt-12'>
+      <div className='bg-gray-100 rounded-md p-6 basis-1/4'>
+        <div className='flex'>
+          <AiFillGithub className='hover:text-rose-500 text-2xl mr-2' />
+          <AiOutlineTwitter className='hover:text-rose-500 text-2xl mr-2' />
+          <AiFillMail className='hover:text-rose-500 text-2xl' />
+        </div>
+        <h1 className='text-3xl font-bold  mt-5'>Contact me</h1>
+        <p className=''>wisdom_13_@naver.com</p>
+      </div>
       <Contact />
     </div >
-
   );
 }
 
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import { AiFillGithub, AiOutlineTwitter, AiFillMail } from 'react-icons/ai';
+'use client';
 
+import { useState } from 'react';
+
+type Form = {
+  from: string;
+  subject: string;
+  message: string;
+}
+
+const INPUT_CLASS = 'w-full text-sm text-slate-900 placeholder-slate-400 rounded-md p-2 px-3 ring-1 ring-slate-200';
 
 export default function Contact() {
-  return (
-    <div className='flex flex-row shadow-xl rounded-md mt-12'>
-      <div className='bg-gray-100 rounded-md p-6 basis-1/4'>
-        <div className='flex'>
-          <AiFillGithub className='hover:text-rose-500 text-2xl mr-2' />
-          <AiOutlineTwitter className='hover:text-rose-500 text-2xl mr-2' />
-          <AiFillMail className='hover:text-rose-500 text-2xl' />
-        </div>
-        <h1 className='text-3xl font-bold  mt-5'>Contact me</h1>
-        <p className=''>wisdom_13_@naver.com</p>
+  const [form, setForm] = useState<Form>({ from: '', subject: '', message: '' });
+  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  }
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+  }
 
-      </div>
-      <div className='p-6 basis-3/4'>
+  return (
+    <>
+      <form className='p-6 basis-3/4' onSubmit={onSubmit}>
         <label className='block font-semibold mb-2'>Your Email</label>
-        <input className='w-full text-sm text-slate-900 placeholder-slate-400 rounded-md p-2 px-3 ring-1 ring-slate-200' type='text' placeholder='Email' />
+        <input className={INPUT_CLASS} type='text' placeholder='Email' name='from' required autoFocus value={form.from} onChange={onChange} />
         <label className='block font-semibold mb-2 mt-3'>Subject</label>
-        <input className='w-full text-sm text-slate-900 placeholder-slate-400 rounded-md p-2 px-3 ring-1 ring-slate-200' type='text' placeholder='제목' />
+        <input className={INPUT_CLASS} type='text' placeholder='제목' name='subject' required autoFocus value={form.subject} onChange={onChange} />
         <label className='block font-semibold mb-2 mt-3'>Message</label>
-        <textarea className='w-full text-sm text-slate-900 placeholder-slate-400 rounded-md p-2 px-3 ring-1 ring-slate-200' placeholder='내용을 입력해주세요.'></textarea>
+        <textarea className={INPUT_CLASS} placeholder='내용을 입력해주세요.' name='message' required autoFocus value={form.message} onChange={onChange} ></textarea>
         <button className='bg-rose-500 text-white text-sm leading-6 font-semibold py-2 px-5 mt-3 rounded-lg float-right'>Submit</button>
-      </div>
-    </div >
+      </form>
+    </>
   );
 }
 

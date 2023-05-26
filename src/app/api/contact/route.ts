d33a87c158207ddd9c696831559ab1c5f381fa13
jhlee <wisdom_13_@naver.com>
2023-05-26 20:21:50 +0900

    이메일 보내기 - 서버 라우트

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,3 +1,4 @@
+import { sendEmail } from '@/service/email';
 import * as yup from 'yup';
 
 const bodySchema = yup.object().shape({
@@ -6,10 +7,19 @@ const bodySchema = yup.object().shape({
   message: yup.string().required(),
 });
 
-export async function POST(request: Request) {
-  if (!bodySchema.isValidSync(request.body)) {
-    return new Response('유효하지 않은 포맷', { status: 400 })
+export async function POST(req: Request) {
+
+  const body = await req.json();
+
+  if (!bodySchema.isValidSync(body)) {
+    return new Response(JSON.stringify({ message: '메일 발송에 실패하였습니다.' }), { status: 400 })
   }
 
-  const { from, subject, message } = request.body;
+  return sendEmail(body)
+    .then(
+      () => new Response(JSON.stringify({ message: '성공적으로 메일을 발송했습니다.' }), { status: 200 })
+    ).catch(error => {
+      console.error(error);
+      return new Response(JSON.stringify({ message: '메일 발송에 실패하였습니다.' }), { status: 500 })
+    })
 }

441c0f784dfb4f2d648e408f8d434c3a3ddd468e
jhlee <wisdom_13_@naver.com>
2023-06-02 21:59:31 +0900

    about 스타일 수정

--- src/components/AboutDetail.tsx
+++ src/components/AboutDetail.tsx
@@ -1,6 +1,6 @@
 export default function AboutDetail() {
   return (
-    <div className='rounded-md mt-6 p-6 bg-slate-100' style={{ width: '1200px' }}>
+    <div className='rounded-md mt-6 p-6 bg-slate-100'>
       <h1 className='text-2xl font-bold'>Who  am I?</h1>
       <p>어쩌고</p>
       <h1 className='text-2xl font-bold mt-4'>Career</h1>
